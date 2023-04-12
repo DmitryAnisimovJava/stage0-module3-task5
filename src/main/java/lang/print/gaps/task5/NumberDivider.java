5e95b75b3dfb4b48cc8fb2f197d0215ffb10894f
Anisi <Anisi@reddiPC>
2023-04-12 21:07:31 +0300

    stage 0 task 5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+    float result, divider = 5;
+    result = toBeDivided / divider;
+    System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+    	float seconds;
+    	seconds = minutes / 60;
     }
 }
